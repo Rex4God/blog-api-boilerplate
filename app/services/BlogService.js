a353d0350ce49af4a5cc07713fbda39b7ce8ff8c
Rex4God <preciousagamuyi@gmail.com>
2025-06-12 16:24:12 +0100

    add some endpoints to the codebase

--- app/services/BlogService.js
+++ app/services/BlogService.js
@@ -2,6 +2,7 @@
 const Blog = require("../models/BlogModel")
 const { StatusCodes } = require("http-status-codes")
 const blogValidator = require("../validators/BlogValidator")
+const { resolveRequestQueryToMongoDBQuery } = require("../utils/helpers")
 
 
 exports.createBlog = async (body, req) => {
@@ -31,10 +32,62 @@ exports.createBlog = async (body, req) => {
       }
 
    } catch (e) {
-      console.log("An unknown error has occurred", +e)
+      console.log("An unknown error has occurred. Please try again later." + e)
       return {
          error: e.message,
          statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
       }
    }
 }
+
+exports.getAllBlogs = async (requestParams) => {
+   try {
+      const mongodbQuery = resolveRequestQueryToMongoDBQuery(requestParams);
+
+      const blogs = await Blog
+         .find(mongodbQuery.filter)
+         .skip(mongodbQuery.page)
+         .sort(mongodbQuery.sort)
+         .limit(mongodbQuery.limit);
+
+
+      return {
+         data: blogs,
+         statusCode: StatusCodes.OK,
+      }
+
+   } catch (e) {
+      console.log("An unknown error has occurred. Please try again later." + e)
+      return {
+         error: e.message,
+         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      }
+   }
+}
+
+exports.getBlog = async (blogId) => {
+   try {
+      const blog = await Blog.findById(blogId)
+
+      if (!blog) {
+         return {
+            error: "Oops! This blog detail is not found on this platform",
+            statusCode: StatusCodes.NOT_FOUND,
+         }
+      }
+
+      return {
+         data: blog,
+         statusCode: StatusCodes.OK,
+      }
+
+   } catch (e) {
+      console.log("An unknown error has occurred. Please try again later." + e)
+      return {
+         error: e.message,
+         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      }
+   }
+}
+
+// COMPLETE THE UPDATE AND DELETE ENDPOINTS FUNCTIONS
